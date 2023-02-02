531c09847d20cbc45473a9a86fb06e71e7f7177c
Mohammad Noruzi <mohammadnorozy77@gmail.com>
2023-02-02 19:54:03 +0330

    cars

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  carsReducer,
+  addCar,
+  removeCar,
+  searchTermChange,
+} from "./slices/carsSlice";
+import { formReducer, changeName, changeCost } from "./slices/formSlice";
+
+const store = configureStore({
+  reducer: {
+    cars: carsReducer,
+    form: formReducer,
+  },
+});
+
+export { store, changeName, changeCost, addCar, removeCar, searchTermChange };
